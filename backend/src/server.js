c2c671248a6f05cc306f7bf15b7c974b90f40905
Ayush13999 <singhalayush13999@gmail.com>
2025-11-10 16:12:56 +0530

    signup feature added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,21 @@ import express from "express"
 import dotenv from "dotenv"
 import path from "path"
 
+dotenv.config();
+
 import authRoutes from"./routes/auth.route.js"
 import messageRoutes from"./routes/message.route.js"
+import {connectDB} from "./lib/db.js"
 
 
-dotenv.config();
 
 
 const app=express()
 const __dirname=path.resolve()
 
 const PORT=process.env.PORT ||3000
+
+app.use(express.json()) //req.body
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
@@ -28,4 +32,7 @@ if(process.env.NODE_ENV=="production"){
 }
 
 
-app.listen(PORT,() => console.log("Server is running on port " + PORT))
+app.listen(PORT,() => {
+    console.log("Server is running on port " + PORT);
+    connectDB()
+})
